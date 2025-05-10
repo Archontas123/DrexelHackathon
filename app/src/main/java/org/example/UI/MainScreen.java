9f08031ab58a6b9468fd51a9578f763621cbb822
Zuhaad Rathore <tavuc123@hotmail.com>
2025-05-10 10:25:42 -0400

    Made a MainScreen thats a blank screen

---
+++ app/src/main/java/org/example/UI/MainScreen.java
@@ -0,0 +1,14 @@
+package org.example.UI;
+
+import javax.swing.JFrame;
+
+public class MainScreen extends JFrame {
+
+    public MainScreen() {
+        setTitle("Main Screen");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); 
+    }
+    
+}
