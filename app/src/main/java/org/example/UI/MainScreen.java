12222a7be7d382d9727930cbf2d4a6e27b70e57f
Zuhaad Rathore <tavuc123@hotmail.com>
2025-05-10 12:10:17 -0400

    Added a Start Screen that allows for grabbing userJSONPath and questionsJSONPath

--- app/src/main/java/org/example/UI/MainScreen.java
+++ app/src/main/java/org/example/UI/MainScreen.java
@@ -3,12 +3,18 @@ package org.example.UI;
 import javax.swing.JFrame;
 
 public class MainScreen extends JFrame {
+    private String questionsJSONPath;
+    private String userJSONPath;
 
-    public MainScreen() {
+    public MainScreen(String questionsJSONPath, String userJSONPath) {
+        this.questionsJSONPath = questionsJSONPath;
+        this.userJSONPath = userJSONPath;
+        
         setTitle("Main Screen");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null); 
+
     }
     
 }
